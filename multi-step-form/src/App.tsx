86245565bbc2ab4fc55884c8b9ffe8d46e667180
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-04 02:00:31 -0300

    add theme provider styled-components

--- multi-step-form/src/App.tsx
+++ multi-step-form/src/App.tsx
@@ -1,12 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { Home } from './pages';
 import { GlobalStyles } from './styles/global';
+import { defaultTheme } from './styles/theme';
 
 function App() {
 
   return (
-    <>
-
+    <ThemeProvider theme={defaultTheme}>
+      <Home />
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   );
 }
 
