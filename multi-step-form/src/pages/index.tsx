187cf10bf0c2537c1133d25ca1906899f8e61095
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-12-04 09:38:19 -0300

    feature aside slide control

--- multi-step-form/src/pages/index.tsx
+++ multi-step-form/src/pages/index.tsx
@@ -1,10 +1,54 @@
+import { useState } from 'react';
 import * as Styled from './styles';
 
+
+const steps = [
+  {
+    id: 1,
+    title: 'your info'
+  },
+  {
+    id: 2,
+    title: 'select plan'
+  },
+  {
+    id: 3,
+    title: 'add-ons'
+  },
+  {
+    id: 4,
+    title: 'summary'
+  },
+];
+
+
 export function Home() {
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  function handleChangeSlideIndex(id: number) {
+    setSlideIndex(id);
+  }
+
   return (
     <Styled.Container>
       <Styled.Content>
-        <Styled.StepController />
+        <Styled.StepController>
+          {steps.map(step => (
+            <Styled.Step key={step.id} isActive={slideIndex === step.id} onClick={() => handleChangeSlideIndex(step.id)}>
+              <div className='circle'>{step.id}</div>
+              <div>
+                <strong>Step {step.id}</strong>
+                <p>{step.title}</p>
+              </div>
+            </Styled.Step>
+          ))}
+
+          <form>
+
+          </form>
+
+        </Styled.StepController>
+
 
       </Styled.Content>
     </Styled.Container>
--- multi-step-form/src/pages/styles.ts
+++ multi-step-form/src/pages/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.main`
 `;
 
 export const Content = styled.div`
-  max-width: 1440px;
+  max-width: 960px;
   width: 100%;
 
   background-color: ${({theme}) => theme.COLORS.white};
@@ -28,6 +28,54 @@ export const StepController = styled.div`
   background-size: cover;
 
   border-radius: 1rem;
-  width: 320px;
-  height: 700px;
+  width: 270px;
+  height: 600px;
+
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+
+interface StepProps {
+  isActive: boolean;
+}
+
+export const Step = styled.div<StepProps>`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  cursor: pointer;
+
+  .circle {
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+    background-color: ${({theme, isActive}) => isActive ? theme.COLORS['blue-light'] : 'transparent'};
+    border: 1px solid ${({theme}) => theme.COLORS['white']};;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 0.875rem;
+    font-weight: ${({theme}) => theme['FONT-WEIGHT'][700]};
+    color: ${({theme}) => theme.COLORS['blue-marine']};
+  }
+
+  strong {
+    font-size: .75rem;
+    color: ${({theme}) => theme.COLORS['gray-light']};
+    text-transform: uppercase;
+  }
+
+  p {
+    font-weight: ${({theme}) => theme['FONT-WEIGHT'][700]};
+    color: ${({theme}) => theme.COLORS['white']};
+    text-transform: uppercase;
+    font-size: .875rem;
+  }
+
 `;
